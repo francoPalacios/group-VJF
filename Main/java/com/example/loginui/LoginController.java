6ee635d7af8b29bdd8112404b6c87251459076be
vathao777 <vathao777@gmail.com>
2024-03-04 14:26:27 -0800

    add budget done some part

--- Main/java/com/example/loginui/LoginController.java
+++ Main/java/com/example/loginui/LoginController.java
@@ -1,5 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.example.loginui;
 
+import java.awt.Component;
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,77 +15,88 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
-import java.io.IOException;
-
-public class LoginController {
+public class LoginController extends Component {
     @FXML
     private TextField emailField;
-
     @FXML
     private PasswordField passwordField;
-
     @FXML
     private Button loginButton;
-
     @FXML
     private Button registerButton;
-
-    private final DatabaseManager databaseManager;
+    private final DatabaseManager databaseManager = new DatabaseManager();
 
     public LoginController() {
-        databaseManager = new DatabaseManager();
     }
 
     @FXML
     void initialize() {
-        loginButton.setOnAction(event -> {
-            String email = emailField.getText();
-            String password = passwordField.getText();
+        this.loginButton.setOnAction((event) -> {
+            String email = this.emailField.getText();
+            String password = this.passwordField.getText();
+            Alert alert;
+            if (!email.isEmpty() && !password.isEmpty()) {
+                if (this.databaseManager.validateUser(email, password)) {
+                    System.out.println("Login successful");
+                    JOptionPane.showMessageDialog(this, "Login Successful", "Success", 1);
+                    this.openBudgetwindow();
+                    Stage loginStage = (Stage)this.loginButton.getScene().getWindow();
+                    loginStage.close();
 
-            if (email.isEmpty() || password.isEmpty()) {
-                Alert alExiAlert = new Alert(Alert.AlertType.ERROR);
-                alExiAlert.setTitle("Error");
-                alExiAlert.setHeaderText(null);
-                alExiAlert.setContentText("Error: Please Fill In All Fields");
-                alExiAlert.showAndWait();
-                return;
-            }
+                } else {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Login Failed");
+                    alert.setHeaderText((String)null);
+                    alert.setContentText("Invalid username or password. Please try again.");
+                    alert.showAndWait();
+                }
 
-            if (databaseManager.validateUser(email, password)) {
-                System.out.println("Login successful");
-                // Perform further actions after successful login
             } else {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Login Failed");
-                alert.setHeaderText(null);
-                alert.setContentText("Invalid username or password. Please try again.");
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText((String)null);
+                alert.setContentText("Error: Please Fill In All Fields");
                 alert.showAndWait();
             }
         });
-
-        registerButton.setOnAction(event -> {
-            // Open the register window
-            openRegisterWindow();
+        this.registerButton.setOnAction((event) -> {
+            this.openRegisterWindow();
         });
     }
 
     private void openRegisterWindow() {
         try {
             Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com.example.loginui/Register.fxml"));
-            Parent root = loader.load();
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/com.example.loginui/Register.fxml"));
+            Parent root = (Parent)loader.load();
             stage.setScene(new Scene(root));
             stage.setTitle("Register");
             stage.setResizable(false);
             stage.show();
-
-            // Close the login window
-            Stage loginStage = (Stage) loginButton.getScene().getWindow();
+            Stage loginStage = (Stage)this.loginButton.getScene().getWindow();
             loginStage.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException var5) {
+            var5.printStackTrace();
         }
+
+    }
+
+    private void openBudgetwindow() {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/com.example.loginui/addbudget.fxml"));
+            Parent root = (Parent)loader.load();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Budget");
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException var4) {
+            var4.printStackTrace();
+        }
+
     }
 }
